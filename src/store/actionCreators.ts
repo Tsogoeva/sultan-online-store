b77bd04c66b54ad33e185091d05a6250bcdc1d36
Tatiana Tsogoeva <tsogoevats@gmail.com>
2023-03-29 22:58:41 +0700

    is done

--- src/store/actionCreators.ts
+++ src/store/actionCreators.ts
@@ -1,15 +1,17 @@
 
+import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 import { IProduct } from "../types/IProduct";
-import { goodsFetching, goodsFetchingFailed, goodsFetchingSuccess } from "./goodSlice";
-import { AppDispatch } from "./reducers";
 
-export const fetchGoods = () => async (dispatch: AppDispatch) => {
-	try {
-		dispatch(goodsFetching())
-		const response = await axios.get<IProduct[]>('http://localhost:5000/goods')
-		dispatch(goodsFetchingSuccess(response.data));
-	} catch (error: any) {
-		dispatch(goodsFetchingFailed(error.message));
+
+export const fetchGoods = createAsyncThunk(
+	'goods/fetchGoods',
+	async (_, { rejectWithValue }) => {
+		try {
+			const response = await axios.get<IProduct[]>('http://localhost:5000/goods');
+			return response.data;
+		} catch (e) {
+			return rejectWithValue('Не удалось загрузить товары!')
+		}
 	}
-}
+);
