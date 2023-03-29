8726f62f4364dd5997a416c191a67f7680f1b7a4
Tatiana Tsogoeva <tsogoevats@gmail.com>
2023-03-29 22:58:18 +0700

    is done

--- src/store/goodSlice.ts
+++ src/store/goodSlice.ts
@@ -1,47 +1,38 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { IProduct } from "../types/IProduct";
+import { fetchGoods } from "./actionCreators";
 
 interface GoodState {
 	goods: IProduct[],
 	isLoading: boolean,
 	error: string
-	// count: number
 }
 
 const initialState: GoodState = {
 	goods: [],
 	isLoading: false,
 	error: ''
-	// count: 0
 }
 
 export const goodSlice = createSlice({
 	name: 'goods',
 	initialState,
-	reducers: {
-		goodsFetching(state) {
-			state.isLoading = true;
-		},
-
-		goodsFetchingSuccess(state, action: PayloadAction<IProduct[]>) {
-			state.isLoading = false;
-			state.error = '';
-			state.goods = action.payload;
-
-		},
-
-		goodsFetchingFailed(state, action: PayloadAction<string>) {
-			state.isLoading = false;
-			state.error = action.payload;
-
-		}
-
-		// increment(state, action: PayloadAction<number>) {
-		// 	state.count += action.payload;
-		// }
-
+	reducers: {},
+	extraReducers: (builder) => {
+		builder
+			.addCase(fetchGoods.pending.type, (state) => {
+				state.isLoading = true;
+			})
+			.addCase(fetchGoods.fulfilled.type, (state, action: PayloadAction<IProduct[]>) => {
+				state.isLoading = false;
+				state.error = '';
+				state.goods = action.payload;
+			})
+			.addCase(fetchGoods.rejected.type, (state, action: PayloadAction<string>) => {
+				state.isLoading = false;
+				state.error = action.payload;
+			})
 	}
 })
 
-export const { goodsFetching, goodsFetchingSuccess, goodsFetchingFailed } = goodSlice.actions;
 export default goodSlice.reducer;
