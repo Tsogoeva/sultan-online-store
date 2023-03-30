b06bc9e5cb9b3317db81d0da0cd09580801e438b
Tatiana Tsogoeva <tsogoevats@gmail.com>
2023-03-31 01:07:25 +0700

    component is ready

---
+++ src/components/UX/Input/Input.tsx
@@ -0,0 +1,20 @@
+import { FC } from "react";
+import styles from './input.module.scss';
+
+interface IInputProps {
+	placeholder: string;
+	icon: string;
+}
+
+const Input: FC<IInputProps> = ({ placeholder, icon }) => {
+	return (
+		<div className={styles.container}>
+			<input className={styles.input} type="text" placeholder={placeholder} />
+			<button className={styles.button} type="submit">
+				<img className={styles.icon} src={icon} alt="Иконка" />
+			</button>
+		</div>
+	)
+}
+
+export default Input;
