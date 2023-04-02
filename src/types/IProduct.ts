532fe7fa8f68c69e17dd65f287f995591189c790
Tatiana Tsogoeva <tsogoevats@gmail.com>
2023-04-02 22:06:57 +0700

    add fields

--- src/types/IProduct.ts
+++ src/types/IProduct.ts
@@ -1,12 +1,14 @@
 export interface IProduct {
-	id: number,
+	id: string,
 	imgUrl: string,
 	title: string,
+	type: string[],
+	subtype: string[],
 	typeSize: string,
 	size: string,
 	barcode: string,
 	manufacturer: string,
 	brand: string,
 	description: string,
-	price: number
+	price: string
 }
