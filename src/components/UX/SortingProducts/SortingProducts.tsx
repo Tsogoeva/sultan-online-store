159d2477a85eddfa41860fb9d3e955aa9ad43623
Tatiana Tsogoeva <tsogoevats@gmail.com>
2023-04-02 22:51:00 +0700

    init

---
+++ src/components/UX/SortingProducts/SortingProducts.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+import styles from './sorting-products.module.scss';
+
+const SortingProducts: FC = () => {
+	return (
+		<div className={styles.container}>
+		</div>
+	)
+}
+
+export default SortingProducts;
