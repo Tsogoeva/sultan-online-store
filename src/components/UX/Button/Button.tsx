72ecb23412bbc38610ea9d70bb0c98a9c2ac87cf
Tatiana Tsogoeva <tsogoevats@gmail.com>
2023-04-02 22:21:04 +0700

    add several options for button size

--- src/components/UX/Button/Button.tsx
+++ src/components/UX/Button/Button.tsx
@@ -4,12 +4,16 @@ import styles from './button.module.scss';
 interface IButtonProps {
 	text: string;
 	icon: string;
+	size: string
 }
 
-const Button: FC<IButtonProps> = ({ text, icon }) => {
+const Button: FC<IButtonProps> = ({ text, icon, size }) => {
+
+	const currentSize = size === 'big' ? styles.big_size : styles.mini_size;
+
 	return (
 		<div className={styles.container}>
-			<button type="button">
+			<button className={currentSize} type="button">
 				{text}
 				<img src={icon} alt="Иконка" />
 			</button>
