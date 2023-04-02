b81843c8e34132a099b45871590c7a5c3086b579
Tatiana Tsogoeva <tsogoevats@gmail.com>
2023-04-02 22:16:52 +0700

    is done

---
+++ src/components/UX/FilterFormByPrice/FilterFormByPrice.tsx
@@ -0,0 +1,49 @@
+import { FC } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+
+import styles from './filter-form-by-price.module.scss';
+
+import { useAppDispatch } from "../../../hooks";
+import { fetchGoods } from "../../../store/actionCreators";
+import { filterByPrice } from "../../../store/goodSlice";
+
+export interface IPriceRange {
+	min: number;
+	max: number;
+}
+
+const defaultValues = { min: 0, max: 10000 }
+
+const FilterFormByPrice: FC = () => {
+	const dispatch = useAppDispatch();
+
+	const {
+		register,
+		handleSubmit,
+	} = useForm<IPriceRange>({ defaultValues });
+
+	const onSubmit: SubmitHandler<IPriceRange> = async (data) => {
+		console.log(data.max)
+
+		// eslint-disable-next-line @typescript-eslint/no-floating-promises
+		await dispatch(fetchGoods())
+		dispatch(filterByPrice({
+			min: data.min || defaultValues.min,
+			max: data.max || defaultValues.max,
+		}));
+	}
+
+	return (
+		// eslint-disable-next-line @typescript-eslint/no-misused-promises
+		<form className={styles.form} onChange={handleSubmit(onSubmit)}>
+			<h4 className={styles.form_title}>Цена <b>₽</b></h4>
+			<div className={styles.inputs}>
+				<input className={styles.input} {...register("min")} placeholder="0" />
+				<i>-</i>
+				<input className={styles.input} {...register("max")} placeholder="10000" />
+			</div>
+		</form>
+	)
+}
+
+export default FilterFormByPrice;
