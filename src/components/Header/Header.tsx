b1c8f8d2608472746cd2036407d1f916cb17ec1e
Tatiana Tsogoeva <tsogoevats@gmail.com>
2023-04-02 22:00:14 +0700

    made minor changes

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,100 +1,61 @@
 import React, { FC } from "react";
 import styles from './header.module.scss';
+
+import Logo from "../UI/Logo/Logo";
+import Button from "../UX/Button/Button";
+import Input from "../UX/Input/Input";
+import ShortInfo from '../UI/ShortInfo/ShortInfo';
+import ShoppingCart from "../UX/ShoppingCart/ShoppingCart";
+
 import iconAddress from './assets/icon-address.svg';
 import iconImail from './assets/icon-email.svg';
 
-import Logo from "../UI/LogoDark/LogoDark";
-
-import Button from "../UX/Button/Button";
 import buttonCatalogIcon from './assets/catalog-button-icon.svg';
 import buttonDownloadIcon from './assets/download-button-icon.svg';
 
-import Input from "../UX/Input/Input";
 import searchIcon from './assets/search-icon.svg';
-
 import operatorImg from './assets/operator.svg';
-
-import ShoppingCart from "../UX/ShoppingCart/ShoppingCart";
 
 
 const Header: FC = () => {
-	const data = {
-		catalog: 'Каталог',
-		download: 'Прайс-лист',
-		placeholder: 'Поиск...'
-	}
-
-
-	const {
-		header,
-		border_container_top,
-		container_top,
-		info,
-		address,
-		address_icon,
-		address_info,
-		address_details,
-		email,
-		email_icon,
-		email_info,
-		email_details,
-		navBar,
-		navBar_item,
-		navBar_item_last,
-		border_container_options,
-		container_options,
-		short_info,
-		info_text,
-		phone_number,
-		time,
-		order,
-		operator,
-		online,
-		border
-	} = styles;
-
 	return (
-		<header className={header}>
-			<div className={border_container_top}>
-				<div className={container_top}>
-					<div className={info}>
-						<div className={address}>
-							<img className={address_icon} src={iconAddress} alt="Icon" />
-							<p className={address_info}>г. Кокчетав, ул. Ж. Ташенова 129Б</p>
-							<p className={address_details}>(Рынок Восточный)</p>
+		<header className={styles.header}>
+			<div className={styles.border_container_top}>
+				<div className={styles.container_top}>
+					<div className={styles.info}>
+						<div className={styles.address}>
+							<img className={styles.address_icon} src={iconAddress} alt="Icon" />
+							<p className={styles.address_info}>г. Кокчетав, ул. Ж. Ташенова 129Б</p>
+							<p className={styles.address_details}>(Рынок Восточный)</p>
 						</div>
-						<div className={email}>
-							<img className={email_icon} src={iconImail} alt="Icon" />
-							<p className={email_info}>opt.sultan@mail.ru</p>
-							<p className={email_details}>На связи в любое время</p>
+						<div className={styles.email}>
+							<img className={styles.email_icon} src={iconImail} alt="Icon" />
+							<p className={styles.email_info}>opt.sultan@mail.ru</p>
+							<p className={styles.email_details}>На связи в любое время</p>
 						</div>
 					</div>
-					<div className={navBar}>
-						<span className={navBar_item}>О компании</span>
-						<span className={navBar_item}>Доставка и оплата</span>
-						<span className={navBar_item}>Возврат</span>
-						<span className={navBar_item_last}>Контакты</span>
+					<div className={styles.navBar}>
+						<span className={styles.navBar_item}>О компании</span>
+						<span className={styles.navBar_item}>Доставка и оплата</span>
+						<span className={styles.navBar_item}>Возврат</span>
+						<span className={styles.navBar_item_last}>Контакты</span>
 					</div>
 				</div>
 			</div>
-			<div className={border_container_options}>
-				<div className={container_options}>
-					<Logo />
-					<Button text={data.catalog} icon={buttonCatalogIcon} />
-					<Input placeholder={data.placeholder} icon={searchIcon} />
-					<div className={short_info}>
-						<div className={info_text}>
-							<p className={phone_number}>+7 (777) 490-00-91</p>
-							<p className={time}>время работы: 9:00-20:00</p>
-							<p className={order}>Заказать звонок</p>
-						</div>
-						<div className={operator}>
+			<div className={styles.border_container_options}>
+				<div className={styles.container_options}>
+					<Logo theme={'dark'} />
+					<Button text={'Каталог'} icon={buttonCatalogIcon} size={'big'} />
+					<Input placeholder={'Поиск...'} icon={searchIcon} />
+					<div className={styles.short_info}>
+						<ShortInfo color={'header'} />
+						<div className={styles.operator}>
 							<img src={operatorImg} alt="Оператор" />
-							<div className={online}></div>
+							<div className={styles.online}></div>
 						</div>
 					</div>
-					<div className={border}>
-						<Button text={data.download} icon={buttonDownloadIcon} />
+					<div className={styles.border}>
+						<Button text={'Прайс-лист'} icon={buttonDownloadIcon} size={'big'} />
 
 					</div>
 					<ShoppingCart />
