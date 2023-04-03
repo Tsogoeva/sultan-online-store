e952d1aaa55934756035ee5fda05be6b8a296b4e
Tatiana Tsogoeva <tsogoevats@gmail.com>
2023-04-03 19:18:35 +0700

    add additional types for the layout

--- src/components/UX/SortingTypeProduct/SortingTypeProduct.tsx
+++ src/components/UX/SortingTypeProduct/SortingTypeProduct.tsx
@@ -1,14 +1,28 @@
 import { FC } from "react";
+import cn from 'classnames';
 import styles from './sorting-type-product.module.scss';
 
 import { useAppSelector } from "../../../hooks";
 
 const SortingTypeProduct: FC = () => {
+	const anotherTypes = [
+		'Освежители воздуха',
+		'Средства по уходу за обувью',
+		'Средсва для мытья полов',
+		'Средства для мытья стёкол',
+		'Чистящие средства для труб'
+	];
+
 	const { types } = useAppSelector(state => state.goodReducer)
 
 	return (
-		<div className={styles.sorting_type_product}>
-			{types.map((type) => <div key={type} className={styles.type}>{type}</div>)}
+		<div className={styles.container}>
+			<div className={styles.sorting_type_product}>
+				{types.map((type) => <div key={type} className={styles.type}>{type}</div>)}
+			</div>
+			<div className={styles.sorting_type_product}>
+				{anotherTypes.map((type) => <div key={type} className={styles.type}>{type}</div>)}
+			</div>
 		</div>
 	)
 }
