9145652059aa04d42a038970eb62291e4151b845
Tatiana Tsogoeva <tsogoevats@gmail.com>
2023-04-05 21:12:45 +0700

    add props and assets

--- src/pages/Product/Product.tsx
+++ src/pages/Product/Product.tsx
@@ -1,10 +1,33 @@
 import React, { FC } from "react";
 import styles from './product.module.scss';
 
-const Product: FC = () => {
+import grams from './assets/type-g.svg';
+import milliliters from './assets/type-ml.svg';
+import { IProduct } from "../../types/IProduct";
+
+interface IProductProp {
+	product: IProduct
+}
+
+const Product: FC<IProductProp> = ({ product }) => {
+	const { title, typeSize, size } = product;
+
 	return (
-		<div className={styles.product}>
-			Карточка товара!
+		<div className={styles.content}>
+			Карточка Товара!
+			<p className={styles.path}>{`Главная / Каталог / ${title}`}</p>
+			<div className={styles.container}>
+				<img src="#" alt="Изображение товара" />
+				<div className={styles.info}>
+					<span className={styles.in_stock}>В наличии</span>
+					<h1 className={styles.title}>{title}</h1>
+					<div className={styles.size}>
+						<img className={styles.icon} src={typeSize === 'г' ? grams : milliliters} alt="Тип" />
+						<span className={styles.count}>{size}</span>
+						<span>{typeSize}</span>
+					</div>
+				</div>
+			</div>
 		</div>
 	)
 }
