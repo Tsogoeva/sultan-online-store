a4a17f2c939b5475ce4fc0d6e9fa2ad89795216e
Tatiana Tsogoeva <tsogoevats@gmail.com>
2023-04-06 04:45:52 +0700

    add link to cart

--- src/components/UX/ButtonToCartFromProduct/ButtonToCartFromProduct.tsx
+++ src/components/UX/ButtonToCartFromProduct/ButtonToCartFromProduct.tsx
@@ -1,16 +1,21 @@
 import { FC } from 'react';
+import { Link } from 'react-router-dom';
 import styles from './button-to-cart-from-product.module.scss';
 
 import cartIcon from './assets/mini-cart-icon.svg';
+
 
 const ButtonToCartFromProduct: FC = () => {
 
 	return (
 		<div className={styles.container}>
-			<button type="button">
-				{'В корзину'}
-				<img src={cartIcon} alt="Иконка" />
-			</button>
+			<Link to={'/sultan-online-store/cart'}>
+				<button type="button">
+					{'В корзину'}
+					<img src={cartIcon} alt="Иконка" />
+				</button>
+			</Link>
+
 		</div>
 	)
 }
