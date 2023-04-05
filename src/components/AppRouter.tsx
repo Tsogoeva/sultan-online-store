f1d490bb5a1baafb7f29c8a3cf60d3080b401d29
Tatiana Tsogoeva <tsogoevats@gmail.com>
2023-04-05 21:00:49 +0700

    add route for every product

--- src/components/AppRouter.tsx
+++ src/components/AppRouter.tsx
@@ -1,17 +1,24 @@
-import React, { FC } from "react";
-import { Route, Routes } from "react-router-dom";
-import Cart from "../pages/Cart/Cart";
-import Product from "../pages/Product/Product";
-import ProductCatalog from "../pages/ProductCatalog/ProductCatalog";
+import React, { FC } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { useAppSelector } from '../hooks';
+
+import Cart from '../pages/Cart/Cart';
+import Product from '../pages/Product/Product';
+import ProductCatalog from '../pages/ProductCatalog/ProductCatalog';
+
 
 const AppRouter: FC = () => {
-
-	const id = 1; // id товара
+	const { goods } = useAppSelector(state => state.goodReducer)
 
 	return (
 		<Routes>
 			<Route path="/" element={<ProductCatalog />} />
-			<Route path={`/product/${id}`} element={<Product />} />
+			{goods.map((product) => <Route
+				path={`/product/${product.id}`}
+				element={<Product product={product}
+				/>}
+			/>
+			)}
 			<Route path="/cart" element={<Cart />} />
 			<Route path="*" element={<ProductCatalog />} />
 		</Routes>
