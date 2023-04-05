1d8983c40296208b94737143913e6ce62bd9226d
Tatiana Tsogoeva <tsogoevats@gmail.com>
2023-04-05 22:31:39 +0700

    stub json-db

--- src/store/actionCreators.ts
+++ src/store/actionCreators.ts
@@ -4,15 +4,23 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 
 import { IProduct } from '../types/IProduct';
 import { ITypes } from './goodSlice';
+import db from './db.json';
+console.log({ db });
 
 export const fetchData = createAsyncThunk(
 	'goods/fetchGoods',
-	async (_, { rejectWithValue }) => {
+	(_, { rejectWithValue }) => {
 
 		try {
-			const responseGoods = await axios.get<IProduct[]>(`http://localhost:5000/goods`);
-			const responseTypes = await axios.get<ITypes[]>(`http://localhost:5000/types`);
-			return { goods: responseGoods.data, types: responseTypes.data };
+			// cant use json-server on github pages
+			// const responseGoods = await axios.get<IProduct[]>(`http://localhost:5000/goods`);
+			// const responseTypes = await axios.get<ITypes[]>(`http://localhost:5000/types`);
+
+			// use stub instead
+			const responseGoods = db.goods;
+			const responseTypes = db.types;
+
+			return { goods: responseGoods, types: responseTypes };
 		} catch (e) {
 			return rejectWithValue('Не удалось загрузить товары!');
 		}
