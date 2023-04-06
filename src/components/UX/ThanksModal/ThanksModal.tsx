cd785c319f7396d5c668f55f97cdf4ad8416f61a
Tatiana Tsogoeva <tsogoevats@gmail.com>
2023-04-06 19:34:26 +0700

    is done

---
+++ src/components/UX/ThanksModal/ThanksModal.tsx
@@ -0,0 +1,41 @@
+import { FC } from 'react';
+import cn from 'classnames';
+import styles from './thanks-modal.module.scss';
+
+import modalIcon from './assets/thanks-icon.svg';
+import closeIcon from './assets/close-modal-icon.svg';
+import { useAppDispatch, useAppSelector } from '../../../hooks';
+import { changeStateModal, resetCart } from '../../../store/goodSlice';
+
+
+const ThanksModal: FC = () => {
+	const { modal } = useAppSelector(state => state.goodReducer);
+	const dispatch = useAppDispatch();
+
+	const getClassname = (suspense: string) => modal
+		? cn(suspense, styles.opened)
+		: cn(suspense, styles.closed);
+
+	const closeHandler = () => {
+		dispatch(changeStateModal(modal));
+		dispatch(resetCart());
+	}
+
+	return (
+		<div className={getClassname(styles.background)}>
+			<div className={getClassname(styles.modal)}>
+				<img onClick={closeHandler} className={styles.close} src={closeIcon} alt="Закрыть" />
+				<div className={styles.modal_container}>
+					<div className={styles.icon}>
+						<img src={modalIcon} alt="Галочка" />
+
+					</div>
+					<h4 className={styles.thanks}>Спасибо за заказ</h4>
+					<p className={styles.comment}>Наш менеджер свяжется с вами в ближайшее время</p>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default ThanksModal;
