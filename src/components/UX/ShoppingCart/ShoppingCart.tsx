b343e510c4f1de13c22481df07b74fea20ac9cc0
Tatiana Tsogoeva <tsogoevats@gmail.com>
2023-04-06 17:06:18 +0700

    add displaying cart totals

--- src/components/UX/ShoppingCart/ShoppingCart.tsx
+++ src/components/UX/ShoppingCart/ShoppingCart.tsx
@@ -2,20 +2,22 @@ import { FC } from "react";
 import styles from './shopping-cart.module.scss';
 
 import cartImg from './assets/shopping-cart.svg';
+import { useAppSelector } from "../../../hooks";
 
 const ShoppingCart: FC = () => {
-	const count = 3;
-	const amount = '10000';
+	const { cart } = useAppSelector(state => state.goodReducer);
+	const totalAmount = cart.reduce((sum, productData) => sum + productData.price, 0);
+	const totalCount = cart.reduce((sum, productData) => sum + productData.count, 0);
 
 	return (
 		<div className={styles.container}>
 			<div className={styles.cart}>
 				<img src={cartImg} alt="Корзина" />
-				<div className={styles.count}>{count}</div>
+				<div className={styles.count}>{totalCount}</div>
 			</div>
 			<div className={styles.amount_container}>
 				<p className={styles.title}>Корзина</p>
-				<p className={styles.amount}>{`${amount} ₽`}</p>
+				<p className={styles.amount}>{`${totalAmount} ₽`}</p>
 			</div>
 		</div >
 	)
