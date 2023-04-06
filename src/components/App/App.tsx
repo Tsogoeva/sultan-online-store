ae7cdc0f049157398d81cbb6c088b0a14aa185b1
Tatiana Tsogoeva <tsogoevats@gmail.com>
2023-04-06 19:36:25 +0700

    add modal and background for modal

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,18 +1,23 @@
 import React, { FC } from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import cn from 'classnames';
 
 import resetStyles from './reset-styles.module.scss';
-import style from './app.module.scss';
+import styles from './app.module.scss';
 
 import Header from '../Header/Header';
 import AppRouter from '../AppRouter';
 import Footer from '../Footer/Footer';
+import { useAppSelector } from '../../hooks';
+import ThanksModal from '../UX/ThanksModal/ThanksModal';
 
 const App: FC = () => {
+  const { modal } = useAppSelector(state => state.goodReducer);
 
   return (
     <BrowserRouter>
-      <div style={resetStyles} className={style.app}>
+      <div style={resetStyles} className={modal ? cn(styles.app, styles.opened) : styles.app}>
+        <ThanksModal />
         <Header />
         <AppRouter />
         <Footer />
