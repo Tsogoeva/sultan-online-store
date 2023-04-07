27c6883bdfa923c8ee6d7a5b60ac9c7474f395a9
Tatiana Tsogoeva <tsogoevats@gmail.com>
2023-04-08 01:45:10 +0700

    add button for management page

--- src/pages/ProductCatalog/ProductCatalog.tsx
+++ src/pages/ProductCatalog/ProductCatalog.tsx
@@ -5,12 +5,22 @@ import SortingProducts from '../../components/UX/SortingProducts/SortingProducts
 import FilterTypeProductHeader from '../../components/UX/FilterTypeProductHeader/FilterTypeProductHeader';
 import CatalogFilter from '../../components/UX/CatalogFilter/CatalogFilter';
 import ProductTable from '../../components/UX/ProductTable/ProductTable';
+import { useNavigate } from 'react-router';
 
 const ProductCatalog: FC = () => {
+	const navigate = useNavigate()
+
+	const clickHandler = () => {
+		navigate('/sultan-online-store/management');
+	}
+
 	return (
 		<div className={styles.product_catalog}>
 			<div className={styles.content}>
-				<span className={styles.path}>Главная / Бытовая химия</span>
+				<div className={styles.header}>
+					<span className={styles.path}>Главная / Бытовая химия</span>
+					<div className={styles.manage} onClick={clickHandler}>Управление товарами</div>
+				</div>
 				<div className={styles.sorting}>
 					<h2 className={styles.category}>Бытовая химия</h2>
 					<SortingProducts />
