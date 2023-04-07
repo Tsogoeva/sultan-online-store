96aef2cec5cc81529984e3dd67d8f58020e069fa
Tatiana Tsogoeva <tsogoevats@gmail.com>
2023-04-08 01:46:29 +0700

    add localStorage

--- src/store/actionCreators.ts
+++ src/store/actionCreators.ts
@@ -4,21 +4,29 @@
 // import { ITypes } from './goodSlice';
 
 import { createAsyncThunk } from '@reduxjs/toolkit';
-
 import db from './db.json';
-console.log({ db });
+
+localStorage.setItem('addedGoods', '[]');
 
 export const fetchData = createAsyncThunk(
 	'goods/fetchGoods',
 	(_, { rejectWithValue }) => {
 
 		try {
+			const storage = localStorage.getItem('addedGoods');
+			let parse: string[] = [];
+
+			if (typeof storage === 'string') {
+				// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+				parse = JSON.parse(storage);
+			}
+
 			// cant use json-server on github pages
 			// const responseGoods = await axios.get<IProduct[]>(`http://localhost:5000/goods`);
 			// const responseTypes = await axios.get<ITypes[]>(`http://localhost:5000/types`);
 
 			// use stub instead
-			const responseGoods = db.goods;
+			const responseGoods = [...db.goods, ...parse];
 			const responseTypes = db.types;
 
 			return { goods: responseGoods, types: responseTypes };
