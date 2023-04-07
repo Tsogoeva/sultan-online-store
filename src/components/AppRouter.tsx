304414c90c3305196629771d0d918d05f1c716a2
Tatiana Tsogoeva <tsogoevats@gmail.com>
2023-04-08 01:36:35 +0700

    add id and route for management

--- src/components/AppRouter.tsx
+++ src/components/AppRouter.tsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import { useAppSelector } from '../hooks';
 
 import Cart from '../pages/Cart/Cart';
+import GoodsManagement from '../pages/GoodsManagement/GoodsManagement';
 import Product from '../pages/Product/Product';
 import ProductCatalog from '../pages/ProductCatalog/ProductCatalog';
 
@@ -14,12 +15,14 @@ const AppRouter: FC = () => {
 		<Routes>
 			<Route path="/" element={<ProductCatalog />} />
 			{goods.map((product) => <Route
+				key={product.id}
 				path={`/sultan-online-store/product/${product.id}`}
 				element={<Product product={product}
 				/>}
 			/>
 			)}
 			<Route path="/sultan-online-store/cart" element={<Cart />} />
+			<Route path="/sultan-online-store/management" element={<GoodsManagement />} />
 			<Route path="*" element={<ProductCatalog />} />
 		</Routes>
 	)
